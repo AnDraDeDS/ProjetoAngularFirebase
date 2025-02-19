3ee6b7ef4a05688456a21e689e4b8d55acd67761
Gabriel Andrade Vieira <101003038+AnDraDeDS@users.noreply.github.com>
2025-02-19 21:06:11 +0000

    aula dia 19-02-25

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
-  },
+    apiKey: "AIzaSyCJKfP3_aE-1aMnXOhDbMoy23q4o_t1ISg",
+     authDomain: "aula03-514e3.firebaseapp.com",
+     projectId: "aula03-514e3",
+     storageBucket: "aula03-514e3.firebasestorage.app",
+     messagingSenderId: "763791279094",
+     appId: "1:763791279094:web:5956a0252e022808d73dd7",
+     measurementId: "G-MQNL1GKY0J"
+   },
   production: false
 };
 
